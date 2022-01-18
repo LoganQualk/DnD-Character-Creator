e23d8181dc4268d55ea13d43457e2c270b3563c3
LoganQualk <loganq@gmail.com>
2022-01-18 03:29:40 -0500

    initial setup

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Placeholder</h1>
+            <div>
+                <Link to="/">Home</Link>
+            </div>
+        </nav>
+    )
+}
