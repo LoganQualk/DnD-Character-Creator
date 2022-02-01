e18e64055e7ba443a8c3936bfc7befc4b0f9f75e
LoganQualk <loganq@gmail.com>
2022-02-01 00:12:00 -0500

    added links to spells that take you to the spell page through the API site

--- src/Spells.js
+++ src/Spells.js
@@ -9,7 +9,10 @@ const Spells = () => {
 
     for (let i = 97; i <= 122; i++) {
         getSpellsByFirstLetter.push(String.fromCharCode(i));
+        console.log("https://open5e.com/spells/" + spellNames[i].replace(/\s/g, ''));
     }
+
+    
 
     return (
         <div>
@@ -19,9 +22,10 @@ const Spells = () => {
                     <div key={index}>
                         <h2>{letter.toUpperCase()}</h2>
                         <div className="spells">
-                            {spellNames.filter(name => name.charAt(0).toLowerCase() === letter).map((spell, index) => <p key={index} title={spell}>{spell}</p>)}
+                            {spellNames.filter(name => name.charAt(0).toLowerCase() === letter).map((spell, index) => <a href={'https://open5e.com/spells/'+spell.replace(/[\n\r\s\t]+/g, '-').replace('\'', '').toLowerCase()} target="_blank" rel="noopener noreferrer"><p key={index} title={spell}>{spell}</p></a>)}
                         </div>
                     </div>
+                    
                 )
             })}
         </div>
