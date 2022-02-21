539c50b08c29b9770025015537f875fcbe57f8e0
LoganQualk <loganq@gmail.com>
2022-02-21 12:28:48 -0500

    modal is working

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
+import Dashboard from "./Dashboard";
+
 const Home = () => {
     return (
         <div id="HomePage">
             <h1>HOMEPAGE</h1>
+            <Dashboard></Dashboard>
         </div>
     )
 }
+
 
 export default Home;
